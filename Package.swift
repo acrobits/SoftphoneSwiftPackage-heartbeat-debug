bfb9daf0358c510c7aebb9c6ec61196a66cbcf79
Stanislav Kutil <skutil@acrobits.cz>
2021-12-10 10:06:32 +0100

    Package updated to 169045

--- Package.swift
+++ Package.swift
@@ -14,11 +14,11 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Softphone",
-            url: "https://dist.acrobits.net/heartbeat-swift-package/debug/Softphone.xcframework-169014.zip",
-            checksum: "d79a693b9f5f737fe89f4a3f7ebd8b5e694958ea356dabae77082808e382926d"),
+            url: "https://dist.acrobits.net/heartbeat-swift-package/debug/Softphone.xcframework-169045.zip",
+            checksum: "36bc42c252a5455e112e7b01809be64ea256db00b099376af1d394b82b342155"),
         .binaryTarget(
             name: "Softphone_Swift",
-            url: "https://dist.acrobits.net/heartbeat-swift-package/debug/Softphone_Swift.xcframework-169014.zip",
-            checksum: "c9e6b877351d394f997f578503d249738d380fabd70c81315bfd40fb45b35349"),
+            url: "https://dist.acrobits.net/heartbeat-swift-package/debug/Softphone_Swift.xcframework-169045.zip",
+            checksum: "e5e3f240fcc473d5c37e110fcd4fcb0339ebefc1ec2fa9324336d10a2ce56441"),
     ]
 )
